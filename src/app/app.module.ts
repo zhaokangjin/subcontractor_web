bb3789cd4fbb5a596210d04e0a7eabc3c04ee214
zhaokangjin <kangjin.zhao@accenture.com>
2017-10-12 04:09:44 +0800

    第一个基于开发环境的NG2版本

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserServiceComponent } from './user/user-service/user-service.component';
+import { UserTestComponent } from './user/user-test/user-test.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserServiceComponent,
+    UserTestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/user/user-service/user-service.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-service',
+  templateUrl: './user-service.component.html',
+  styleUrls: ['./user-service.component.scss']
+})
+export class UserServiceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
